dfb3644ee900c91e8800ecdefa92b152cf40cb54
donghyeon0723 <edusolution0723@gmail.com>
2021-03-08 09:50:37 +0900

    방금 알아낸 정보 필드를 이용한 의존성 주입은 바람직 하지 않다. 이유는 1. 의존성을 마음대로 변경할 수 있게 된다. 2. 의존성 정보를 숨기기 어렵다. 3. 단일 책임 원칙을 위반할 가능성이 있다.(하나의 객체는 하나의 책임만 져야한다.)
    생성자를 통해서 의존성을 주입하는 경우 초기화 이후 읽기 전용이 되어서 아무도 그 의존성을 변경할 수 없다.

--- src/main/java/app/messages/MessageService.java
+++ src/main/java/app/messages/MessageService.java
@@ -5,14 +5,15 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MessageService {
-    @Autowired
+    /*@Autowired
+    private MessageRepository repository;*/
     private MessageRepository repository;
 
     //생성시 레포지터리를 주입받음 => 의존성을 주입받음. 제어의 역전
-    /*@Autowired
+    @Autowired
     public MessageService(MessageRepository repository) {
         this.repository = repository;
-    }*/
+    }
     // 데이터를 받으면 메세지 객체를 만들어 전달한다.
     public void save(String text) {
         this.repository.saveMessage(new Message(text));
