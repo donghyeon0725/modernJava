c5fc3eaea4a82752eaa8587a450452d35720f7ed
donghyeon <ehdgus5015@gmail.com>
2021-03-09 16:10:11 +0900

    부트, 소스 수정

--- src/main/java/app/messages/MessageController.java
+++ src/main/java/app/messages/MessageController.java
@@ -1,17 +1,26 @@
 package app.messages;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/messages")
 public class MessageController {
+    /*@GetMapping("/welcome")
+    public String welcome(Model model) {
+        model.addAttribute("message", "Hello, Welcome to Spring Boot!");
+        return "welcome";
+    }*/
+
     @GetMapping("/welcome")
-    @ResponseBody // 리턴 타입이 경로가 아닌, Stream을 타고 나갈 수 있도록 설정함. 원래는 HttpServletResponse 객체를 통해 얻은 스트림으로 해야하는 동작을 프레임워크단에서 처리해줌
-    public String welcome() {
-        return "<strong>Hello, Welcome to Spring Boot!</strong>";
+    public ModelAndView welcome() {
+        ModelAndView mv = new ModelAndView("welcome");
+        mv.addObject("message", "Hello, Welcome to Spring Boot!");
+        return mv;
     }
+
 
 }
